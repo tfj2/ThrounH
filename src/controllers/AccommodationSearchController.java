9aa289c6fae63bc793fa859f416e05202337ff2f
unknown <tfj2@hi.is>
2021-03-10 14:36:14 +0000

    bætti við öllum controllers, skilgreindi methods í AccSearch

---
+++ src/controllers/AccommodationSearchController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+public class AccommodationSearchController {
+
+    public void addManagerId(User user){
+
+    }
+
+    public void addRoom(Room room){
+
+    }
+
+    public void removeRoom(Room room){
+
+    }
+
+    public void changeRoomPrice(Room room, double price){
+
+    }
+
+    public void changeRoomType(Enum roomType){
+
+    }
+
+    public void changeRoomCapacity(Room room, int cap){
+
+    }
+
+    public void changeLocation(Accommodation acc, String Location){
+
+    }
+
+    public void changeFacilities(Accommodation acc, String facilities){ //hvernig geyma facilities?
+
+    }
+
+    public void removeManagerId(User user){
+
+    }
+
+    public void addNewAccommodation(//Þarf að hugsa params){
+
+    }
+
+}
