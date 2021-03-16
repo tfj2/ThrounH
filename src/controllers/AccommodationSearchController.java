0c8e3608ff1e9cf29d69ed78de6555f6842685d3
Daníel Helgi Ágústsson <danielhelgi1@gmail.com>
2021-03-16 19:00:06 +0000

    stick of resolve has resolved

--- src/controllers/AccommodationSearchController.java
+++ src/controllers/AccommodationSearchController.java
@@ -1,13 +1,33 @@
 package controllers;
 
 import entities.Accommodation;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 
 // setti inn return null til að losa um red ~ hell.
-public class AccommodationSearchController {
+public class AccommodationSearchController implements Initializable {
+    @FXML
+    private Button searchButton;
+    @FXML
+    private ListView hotelList;
+    @FXML
+    private TextField hotelTextField;
 
+    //o.fl.
+    // breyta type í user eða annað
+    private ObservableList<String> fyrstiListi = FXCollections.observableArrayList();
+
+    // private DataFactory = new DataFactory();
 
     public ArrayList<Accommodation> findByLocation() {
         return null;
@@ -34,7 +54,14 @@ public class AccommodationSearchController {
         System.out.println("We do be searching tho");
     }
 
-    public static void main (String[] args) {
-        System.out.println("We in this b");
+    @Override
+    public void initialize(URL LOCATION, ResourceBundle resources) {
+        System.out.println("out");
+        fyrstiListi.add("Hallo");
+        fyrstiListi.add("er");
+        fyrstiListi.add("rett");
+
+        hotelList.setItems(fyrstiListi);
     }
+
 }
