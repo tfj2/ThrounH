c43bc96c2e8c0e352bb05f11b90cd14ef28827ff
Daníel Helgi Ágústsson <danielhelgi1@gmail.com>
2021-04-13 16:47:05 +0000

    held lagad

--- src/storage/Database.java
+++ src/storage/Database.java
@@ -5,7 +5,7 @@ import entities.Accommodation;
 import java.sql.Date;
 import java.util.ArrayList;
 
-public interface Database {
+public interface Database  {
     ArrayList<Accommodation> getAllHotels();
     ArrayList<Accommodation> getHotelsByLocation(String location);
     ArrayList<Accommodation> getHotelsByTimePeriod(Date from, Date to);
